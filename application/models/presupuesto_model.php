509f3ad4887f10862a1be73bd20ee31c6f9c2b91
jose aular <jvaular44@gmail.com>
2015-03-05 09:27:09 -0400

    Consulta del detallado de modificacion presupuestaria

--- application/models/presupuesto_model.php
+++ application/models/presupuesto_model.php
@@ -50,3 +50,61 @@ class Presupuesto_model extends CI_Model
  
 /* End of file presupuesto_model.php */
 /* Location: ./application/models/presupuesto_model.php */
+
+ }
+ //function Modificacion_detallada($anio)
+    {
+//SELECT   modificacion . cod_organismo,   
+  //       modificacion . cod_ano,   
+    //     modificacion . tipo_documento,   
+      //   modificacion . nro_modificacion,   
+        // modificacion . flag_reserva,   
+         //modificacion . concepto,   
+         //modificacion . monto,   
+         //modificacion . fecha_documento,   
+         //modificacion . fecha_proceso,   
+         //modificacion . fecha_aprobacion,   
+         //modificacion . fecha_afectacion,   
+         //modificacion . status_m,   
+         //modificacion . fecha_anulacion,   
+         //modificacion . flag_interna_externa,   
+         //modificacion . hora_proceso,   
+         //tipo_de_documento . tipo_documento,   
+         //tipo_de_documento . descripcion_documento,   
+         //modificacion_partida . cod_programa,   
+         //modificacion_partida . cod_subprograma,   
+         //modificacion_partida . cod_proyecto,   
+         //modificacion_partida . cod_act_obra,   
+         //modificacion_partida . cod_cuenta,   
+         //modificacion_partida . ordinal_presupuesto,   
+         //modificacion_partida . monto,   
+         //modificacion_partida . signo,   
+         //presupuesto . descripcion  
+    //FROM modificacion,   
+      //   tipo_de_documento,   
+        // modificacion_partida,   
+         //presupuesto  
+   //WHERE modificacion . tipo_documento = tipo_de_documento . tipo_documento and  
+   //      modificacion_partida . cod_organismo = modificacion . cod_organismo and  
+   //      modificacion_partida . cod_ano = modificacion . cod_ano  and  
+   //      modificacion_partida . tipo_documento = modificacion . tipo_documento  and  
+   //      modificacion_partida . nro_modificacion = modificacion . nro_modificacion  and  
+   //      presupuesto . cod_sector = modificacion_partida . cod_sector  and  
+   //      presupuesto . cod_organismo = modificacion_partida . cod_organismo  and  
+   //      presupuesto . cod_ano = modificacion_partida . cod_ano  and  
+   //      presupuesto . cod_programa = modificacion_partida . cod_programa  and  
+   //      presupuesto . cod_subprograma = modificacion_partida . cod_subprograma and  
+   //      presupuesto . cod_proyecto = modificacion_partida . cod_proyecto and  
+   //      presupuesto . cod_act_obra = modificacion_partida . cod_act_obra  and  
+   //      presupuesto . cod_cuenta = modificacion_partida . cod_cuenta  and  
+   //      presupuesto . ordinal_presupuesto = modificacion_partida . ordinal_presupuesto 
+   
+//ORDER BY  tipo_de_documento . descripcion_documento,   
+  //       modificacion . nro_modificacion ,   
+    //     modificacion . cod_organismo ,   
+      //   modificacion . cod_ano   
+        
+
+        
+}
+ 
