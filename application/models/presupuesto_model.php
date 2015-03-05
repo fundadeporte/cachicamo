64c3b9901d5ac85773ec675ff955268d641f8b2d
jose aular <jvaular44@gmail.com>
2015-03-05 09:14:51 -0400

    Reporte de modificacion presupuestaria

--- application/models/presupuesto_model.php
+++ application/models/presupuesto_model.php
@@ -20,7 +20,32 @@ class Presupuesto_model extends CI_Model
         $query = $saacp->select('nro_modificacion, concepto, monto, fecha_documento, fecha_proceso, status_m, hora_proceso')->get('modificacion');
         return $query->result();
     }
- 
+ //function reporte_modificacion($anio)
+    {
+
+    //SELECT modificacion . nro_modificacion,   
+      // modificacion . cod_ano,   
+       //modificacion . fecha_documento,   
+       //modificacion . flag_interna_externa,
+       //modificacion . flag_reserva, 
+       //modificacion . fecha_aprobacion,   
+       //modificacion . fecha_anulacion, modificacion . monto, 
+       //tipo_de_documento . descripcion_documento,
+       //modificacion . status_m 
+    //FROM modificacion,   
+         //tipo_de_documento, 
+         //modifica_modifica 
+//WHERE modificacion . tipo_documento = tipo_de_documento . tipo_documento and
+//modificacion . tipo_documento = modifica_modifica . mod_tipo_documento and
+//modificacion . nro_modificacion = modifica_modifica . nro_modificacion and
+//modificacion . cod_ano = modifica_modifica . mod_cod_ano and
+//modificacion . cod_organismo = modifica_modifica . cod_organismo and
+
+//order by tipo_de_documento . descripcion_documento,
+  //       modificacion . nro_modificacion
+
+// Consulta de reporte de modificacon presupuestaria
+//      
 }
  
 /* End of file presupuesto_model.php */
